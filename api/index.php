847f50fd52953d0d8463b658336b4a1a2bbc58ac
Freja Fischer <frej657e@stud.kea.dk>
2025-03-12 10:02:17 +0100

    added Hateoas for all api calls

--- api/index.php
+++ api/index.php
@@ -18,6 +18,7 @@ if (!isset($_GET['entity']) || !isset($_GET['information'])) {
             switch ($information) {
                 case 'authors':
                     echo json_encode($catalog->getAuthors());
+                    echo Utils::addHATEOAS($information,$entity);
                     break;
                 default:
                     Utils::leave();
@@ -29,15 +30,17 @@ if (!isset($_GET['entity']) || !isset($_GET['information'])) {
             } else {
                 require_once __DIR__ . '/../classes/author.php';
                 $name = $_GET['name'];
-                $name = str_replace('-', ' ', $name);
+                $formattedName = str_replace('-', ' ', $name);
 
-                $author = new Author($name);
+                $author = new Author($formattedName);
                 switch ($information) {
                     case 'info':
                         echo json_encode($author->getInfo());
+                        echo Utils::addHATEOAS($information,$entity,['author_name'=>$name]);
                         break;
                     case 'poems':
                         echo json_encode($author->getPoems());
+                        echo Utils::addHATEOAS($information,$entity,['author_name'=>$name]);
                         break;
                     default:
                         Utils::leave();
@@ -49,17 +52,18 @@ if (!isset($_GET['entity']) || !isset($_GET['information'])) {
                 Utils::leave();
             } else {
                 require_once __DIR__ . '/../classes/poem.php';
-                $authorName = $_GET['authorName'];
-                $authorName = str_replace('-', ' ', $authorName);
+                $name = $_GET['authorName'];
+                $formattedName = str_replace('-', ' ', $name);
                 $title = $_GET['title'];
                 // Original title: I heard a Fly buzz - when I died -
                 // Title in API request: I-heard-a-Fly-buzz-/-when-I-died-/
-                $title = str_replace('-', ' ', $title);
-                $title = str_replace('/', '-', $title);
-                $poem = new Poem($authorName, $title);
+                $formattedTitle = str_replace('-', ' ', $title);
+                $formattedTitle = str_replace('/', '-', $formattedTitle);
+                $poem = new Poem($formattedName, $formattedTitle);
                 switch ($information) {
                     case 'text':
                         echo json_encode($poem->getText());
+                        echo Utils::addHATEOAS($information,$entity,['title'=>$title, 'author_name'=>$name]);
                         break;
                     default: 
                         Utils::leave();
--- classes/utils.php
+++ classes/utils.php
@@ -1,9 +1,99 @@
 <?php
 
+use Dom\Entity;
+
 class Utils 
 {
     public static function leave() 
     {
         header('Location: ..');
     }
+
+     /**
+     * Adds HATEOAS links to the data it receives as a parameter
+     * 
+     * @param array|string $information Entity information to add the HATEOAS links to
+     * @param string $entity Name of the entity the HATEOAS links will be added to.
+     *                        If nonexistent, only the HATEOAS links will be returned
+     * @param array $urlParameters - Contains extra url parameters if present
+     * @return string The information to be served by the API including its 
+     *          corresponding HATEOAS links encoded as JSON
+     */
+    public static function addHATEOAS(array|string $information = '', string $entity = '', array $urlParameters = []): string 
+    {
+        // entity: catalog, author, poem
+        // information: authors, info, poems, text
+
+        // $curDir = self::urlPath();
+
+        // if urlParamters is present, then exstract content
+        if(!empty($urlParameters)) {
+            $author = $urlParameters['author_name'] ?? '';
+            $title = $urlParameters['title'] ?? '';
+        }
+
+        switch($entity) {
+            case 'catalog':
+                $self = '/authors';
+                break;
+            case 'author':
+                if($information === 'info') {
+                    $self = '/authors/' . $author;
+                } else {
+                    $self = '/authors/' . $author . '/poems';
+                }
+                break;
+            case 'poem':
+                $self = '/authors/' . $author . '/poems/' . $title;
+                break;
+            default:
+                $self = 'undefined';
+        }
+
+        $apiInfo['_links'] = array(
+            array(
+                'self' => $self,
+                'type' => 'GET'
+            )
+        );        
+        return json_encode($apiInfo);
+
+        // How I wish the hateoas design should be:
+        //authors
+        // “_links”: {
+        // 	“self”: “/authors”
+        // }
+
+        // authors/<author_name>
+        // “_links": {
+        //     "self": "/authors/<author_name>”,
+        //     "all_authors": "/authors",
+        //     "poems": "/authors/<author_name>/poems"
+        //   }
+
+        // authors/<author_name>/poems
+        // "links": {
+        //     "self": "/authors/<author_name>/poems",
+        //     "all_authors": "/authors",
+        //     "author": "/authors/<author_name>"
+        //   }
+
+        // authors/<author_name>/poems/<poem_name>
+        // "links": {
+        //     "self": "/authors/<author_name>/poems/<poem_name>”,
+        //     "all_authors": "/authors",
+        //     "author": "/authors/<author_name>",
+        //     "all_poems": "/authors/<author_name>/poems"
+        //   }
+
+    }
+      /**
+     * Returns the API's URL path
+     */
+    // private static function urlPath(): string 
+    // {
+    //     $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') 
+    //         || $_SERVER['SERVER_PORT'] == 443) ? 'https://' : 'http://';
+    //     return $protocol . $_SERVER['HTTP_HOST'] . '/' . 'api' . '/';     
+    // }
 }
